be5d214f9d0a7c7995bdea68051a1fc7fe85d2ea
Romeo Maignal <romeo.maignal@epfl.ch>
2025-11-04 10:34:57 +0100

    updated config files

--- .toggle-dark-mode.sh
+++ .toggle-dark-mode.sh
@@ -5,8 +5,8 @@ current_theme=$(gsettings get org.gnome.desktop.interface color-scheme)
 
 if [[ "$current_theme" == "'prefer-dark'" ]]; then
     gsettings set org.gnome.desktop.interface color-scheme 'default'
-#    gsettings set org.gnome.shell.ubuntu color-scheme 'prefer-light'
+    export THEME="default"
 else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
-#    gsettings set org.gnome.shell.ubuntu color-scheme 'prefer-dark'
+    export THEME="dark"
 fi
